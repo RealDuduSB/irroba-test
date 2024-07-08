05b8d65e0176760a54b030df5491c86ed2ecf0a9
Eduardo Borges <73324166+RealDuduSB@users.noreply.github.com>
2024-07-08 08:27:37 -0300

    Update api_endpoints.dart

--- lib/utils/api_endpoints.dart
+++ lib/utils/api_endpoints.dart
@@ -11,102 +11,102 @@ class API {
 
   // Category endpoints
   static const String GET_CATEGORY = '$BASE_URL/category';
-  static String getCategoryById(String id) => '$BASE_URL/category/$id';
-  static String getCategoryByReference(String reference) =>
+  static String GET_CATEGORY_BY_ID(String id) => '$BASE_URL/category/$id';
+  static String GET_CATEGORY_BY_REFERENCE(String reference) =>
       '$BASE_URL/category/reference/$reference';
   static const String POST_CATEGORY = '$BASE_URL/category';
 
   // Customer endpoints
   static const String GET_CUSTOMER = '$BASE_URL/customer';
-  static String getCustomerById(String id) => '$BASE_URL/customer/$id';
-  static String getCustomerAddressList(String id) =>
+  static String GET_CUSTOMER_BY_ID(String id) => '$BASE_URL/customer/$id';
+  static String GET_CUSTOMER_ADDRESS_LIST(String id) =>
       '$BASE_URL/customer/address_list/$id';
-  static String getCustomerByCpfCnpj(String cpfCnpj) =>
+  static String GET_CUSTOMER_BY_CPF_CNPJ(String cpfCnpj) =>
       '$BASE_URL/customer/cpf_cnpj/$cpfCnpj';
   static const String GET_ABANDONED_CART = '$BASE_URL/customer/abandoned_cart';
-  static String getAbandonedCartById(String id) =>
+  static String GET_ABANDONED_CART_BY_ID(String id) =>
       '$BASE_URL/customer/abandoned_cart/$id';
   static const String POST_CUSTOMER = '$BASE_URL/customer';
-  static String putCustomerById(String id) => '$BASE_URL/customer/$id';
+  static String PUT_CUSTOMER_BY_ID(String id) => '$BASE_URL/customer/$id';
 
   // Customer group endpoints
   static const String GET_CUSTOMER_GROUP = '$BASE_URL/customer/group';
   static const String POST_CUSTOMER_GROUP = '$BASE_URL/customer/group';
-  static String putCustomerGroupById(String id) =>
+  static String PUT_CUSTOMER_GROUP_BY_ID(String id) =>
       '$BASE_URL/customer/group/$id';
 
   // Address endpoints
   static const String POST_ADDRESS = '$BASE_URL/address';
-  static String deleteAddressById(String id) => '$BASE_URL/address/$id';
+  static String DELETE_ADDRESS_BY_ID(String id) => '$BASE_URL/address/$id';
 
   // Manufacturer endpoints
   static const String GET_MANUFACTURER = '$BASE_URL/manufacturer';
-  static String getManufacturerById(String id) => '$BASE_URL/manufacturer/$id';
+  static String GET_MANUFACTURER_BY_ID(String id) => '$BASE_URL/manufacturer/$id';
   static const String POST_MANUFACTURER = '$BASE_URL/manufacturer';
-  static String putManufacturerById(String id) => '$BASE_URL/manufacturer/$id';
-  static String deleteManufacturerById(String id) =>
+  static String PUT_MANUFACTURER_BY_ID(String id) => '$BASE_URL/manufacturer/$id';
+  static String DELETE_MANUFACTURER_BY_ID(String id) =>
       '$BASE_URL/manufacturer/$id';
 
   // Coupon endpoints
   static const String GET_COUPON = '$BASE_URL/coupon';
-  static String getCouponByCode(String code) => '$BASE_URL/coupon/$code';
+  static String GET_COUPON_BY_CODE(String code) => '$BASE_URL/coupon/$code';
   static const String POST_COUPON = '$BASE_URL/coupon';
 
   // Option endpoints
   static const String GET_OPTION = '$BASE_URL/option';
   static const String POST_OPTION = '$BASE_URL/option';
-  static String putOptionVariationById(String id) =>
+  static String PUT_OPTION_VARIATION_BY_ID(String id) =>
       '$BASE_URL/option/variation/$id';
-  static String deleteOptionVariationById(String id) =>
+  static String DELETE_OPTION_VARIATION_BY_ID(String id) =>
       '$BASE_URL/option/variation/$id';
 
   // Product endpoints
   static const String GET_PRODUCTS = '$BASE_URL/products';
-  static String getProductStock() => '$BASE_URL/product';
-  static String getProductByIdOrModel(String idOrModel) =>
+  static String GET_PRODUCT_STOCK() => '$BASE_URL/product';
+  static String GET_PRODUCT_BY_ID_OR_MODEL(String idOrModel) =>
       '$BASE_URL/product/$idOrModel';
-  static String getProductByModel(String model) =>
+  static String GET_PRODUCT_BY_MODEL(String model) =>
       '$BASE_URL/product/$model/model';
   static const String GET_PRODUCT_LISTS = '$BASE_URL/product/lists';
-  static String getProductByCategory(String id) =>
+  static String GET_PRODUCT_BY_CATEGORY(String id) =>
       '$BASE_URL/product/category/$id';
   static const String POST_PRODUCT = '$BASE_URL/product';
-  static String putProductByIdOrModel(String idOrModel) =>
+  static String PUT_PRODUCT_BY_ID_OR_MODEL(String idOrModel) =>
       '$BASE_URL/product/$idOrModel';
-  static String putProductModelByModel(String model) =>
+  static String PUT_PRODUCT_MODEL_BY_MODEL(String model) =>
       '$BASE_URL/product/$model/model';
-  static String putProductStockByIdOrModel(String idOrModel) =>
+  static String PUT_PRODUCT_STOCK_BY_ID_OR_MODEL(String idOrModel) =>
       '$BASE_URL/product/$idOrModel/stock';
-  static String putProductModelStockByModel(String model) =>
+  static String PUT_PRODUCT_MODEL_STOCK_BY_MODEL(String model) =>
       '$BASE_URL/product/$model/model/stock';
-  static String putProductStockByReference(String reference) =>
+  static String PUT_PRODUCT_STOCK_BY_REFERENCE(String reference) =>
       '$BASE_URL/product/stock/reference/$reference';
   static const String PUT_PRODUCT_STOCK_BATCH = '$BASE_URL/product/stock/batch';
-  static String putProductPriceByIdOrModel(String idOrModel) =>
+  static String PUT_PRODUCT_PRICE_BY_ID_OR_MODEL(String idOrModel) =>
       '$BASE_URL/product/$idOrModel/price';
-  static String putProductModelPriceByModel(String model) =>
+  static String PUT_PRODUCT_MODEL_PRICE_BY_MODEL(String model) =>
       '$BASE_URL/product/$model/model/price';
-  static String deleteProductById(String id) => '$BASE_URL/product/$id';
-  static String deleteProductByModel(String model) =>
+  static String DELETE_PRODUCT_BY_ID(String id) => '$BASE_URL/product/$id';
+  static String DELETE_PRODUCT_BY_MODEL(String model) =>
       '$BASE_URL/product/$model';
 
   // Order endpoints
   static const String GET_ORDERS = '$BASE_URL/order';
-  static String getOrderById(String id) => '$BASE_URL/order/$id';
-  static String updateOrderStatus(String id) => '$BASE_URL/order/$id';
+  static String GET_ORDER_BY_ID(String id) => '$BASE_URL/order/$id';
+  static String UPDATE_ORDER_STATUS(String id) => '$BASE_URL/order/$id';
   static const String INIT_ORDER_DATA = '$BASE_URL/initdata';
-  static String getOrderStatusById(String id) => '$BASE_URL/order/status/$id';
-  static String getOrdersAddedByDate(String date) =>
+  static String GET_ORDER_STATUS_BY_ID(String id) => '$BASE_URL/order/status/$id';
+  static String GET_ORDERS_ADDED_BY_DATE(String date) =>
       '$BASE_URL/order/added/$date';
-  static String getOrdersAddedBetweenDates(String dateIni, String dateEnd) =>
+  static String GET_ORDERS_ADDED_BETWEEN_DATES(String dateIni, String dateEnd) =>
       '$BASE_URL/order/added/$dateIni/$dateEnd';
   static const String GET_ORDER_DETAILS = '$BASE_URL/order/details';
-  static String getOrderByCustomerId(String id) =>
+  static String GET_ORDER_BY_CUSTOMER_ID(String id) =>
       '$BASE_URL/order/customer/$id';
-  static String postOrderInvoiceById(String id) =>
+  static String POST_ORDER_INVOICE_BY_ID(String id) =>
       '$BASE_URL/order/$id/invoice';
-  static String putOrderStatusById(String id) => '$BASE_URL/order/$id/status';
-  static String putOrderReferenceById(String id) =>
+  static String PUT_ORDER_STATUS_BY_ID(String id) => '$BASE_URL/order/$id/status';
+  static String PUT_ORDER_REFERENCE_BY_ID(String id) =>
       '$BASE_URL/order/$id/reference';
 
   // Affiliate endpoints
